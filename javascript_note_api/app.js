f4db622071147a635739f1ae7c383cc6401a99f4
Mônica Marçal <monicagmarcal@outlook.com>
2023-04-14 15:44:51 -0300

    feat
    criando uma nota

--- javascript_note_api/app.js
+++ javascript_note_api/app.js
@@ -4,6 +4,7 @@ const logger = require('morgan');
 require ('./config/database');
 
 const usersRouter = require('./app/routes/users');
+const notesRouter = require('./app/routes/notes');
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/users', usersRouter);
+app.use('/notes', notesRouter);
 
 module.exports = app;
