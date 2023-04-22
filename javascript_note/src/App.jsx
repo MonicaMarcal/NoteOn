ad8ee46a81b30354050493b29f5cf171d32f98d1
Mônica Marçal <monicagmarcal@outlook.com>
2023-04-22 15:18:19 -0300

    Tela Inicial/Home

--- javascript_note/src/App.jsx
+++ javascript_note/src/App.jsx
@@ -1,10 +1,10 @@
 import {Fragment } from 'react';
 import "./App.scss";
-import Header from "./components/Header";
+import HomeScreen from "./screens/home";
 
 const App = () => (
     <Fragment>
-      <Header/>
+      <HomeScreen/>
     </Fragment>
  )
 
---
+++ javascript_note/src/screens/home/index.jsx
@@ -0,0 +1,35 @@
+import { Fragment } from 'react';
+import PresentationImage from '../../assets/images/presentation.png';
+import Header from "../../components/header";
+import { Section,Container,Heading} from 'react-bulma-components';
+import "../../styles/home.scss";
+
+const HomeScreen = () => (
+ <Fragment>
+    <Header/>
+    <Section size="medium" className="home">
+      <Container>
+      <div className="columns">
+              <div size={5}>
+                  <Heading size={2} spaced className="has-text-white">
+                      Create notes easily and access when you wants on the cloud
+                  </Heading>
+                  <Heading size={5} spaced className="has-text-light" subtitle>
+                      Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.<br/><br/>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print.
+                  </Heading>
+                    <a className="button is-outlined is-white is-large">
+                          <strong>Register for free Nowstrong</strong>
+                    </a>
+              </div>
+
+              <div size={6} offset={1}>
+                  <img src={PresentationImage} />
+              </div>
+
+        </div>
+      </Container>
+  </Section>
+ </Fragment>
+)
+
+export default HomeScreen;
