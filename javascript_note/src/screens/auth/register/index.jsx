6025cf969009ffa1a850a7d5783084ade51c0238
Mônica Marçal <monicagmarcal@outlook.com>
2023-04-25 15:38:12 -0300

    style
    layout tela register

--- javascript_note/src/screens/auth/register/index.jsx
+++ javascript_note/src/screens/auth/register/index.jsx
@@ -1,8 +1,36 @@
 import { Fragment } from 'react';
+import Header from "../../../components/header";
+import { Section, Container, Card} from 'react-bulma-components';
+import LogoImage from '../../../assets/images/logo.png';
+import "../../../styles/auth.scss";
 
 const RegisterScreen = () => (
  <Fragment>
-   Hello World register
+   <Header/>
+    <Section size="medium" className="auth">
+      <Container>
+        <div>
+          <div size={3}>
+          <Card>
+            <Card.Content>
+              <Section>
+                <div>
+                  <div size={12}  className="has-text-centered">
+                       <img src={LogoImage}/>
+                  </div>
+                  <div size={12}  className="has-text-centered">
+                      <h2 size={6} className="has-text-grey has-text-centered">
+                        Your notes on the cloud
+                      </h2>
+                  </div>
+                  </div>
+              </Section>
+              </Card.Content>
+            </Card>
+          </div>
+        </div>
+      </Container>
+    </Section>
  </Fragment>
 );
 
