fd690071f5ac703e91c6e392f5b306a446b92f1e
Mônica Marçal <monicagmarcal@outlook.com>
2023-05-01 15:46:10 -0300

    style
    nav burguer

--- javascript_note/src/components/header_logged/index.jsx
+++ javascript_note/src/components/header_logged/index.jsx
@@ -27,12 +27,16 @@ function HeaderLogged() {
     <Link to="/notes">
        <img src={LogoImage} />
       </Link>
+      <a
+         className="navbar-burger burger"
+         aria-label="menu"
+         aria-expanded="false"
+         data-target="navbar-menu">
+         <span aria-hidden="true"></span>
+         <span aria-hidden="true"></span>
+         <span aria-hidden="true"></span>
+     </a>
 
-    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
   </div>
 
       <div className="navbar-end">
