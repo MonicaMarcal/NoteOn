7f36f07f076e5e439ff57855a288e4437e0fd692
Mônica Marçal <monicagmarcal@outlook.com>
2023-05-02 15:55:37 -0300

    style
    menu sidebar

--- javascript_note/src/components/notes/index.jsx
+++ javascript_note/src/components/notes/index.jsx
@@ -1,10 +1,36 @@
 import {Fragment } from 'react';
 import '../../styles/notes.scss'
+import { push as Menu } from 'react-burger-menu'
 
-function Notes() {
+
+const Notes = (props) => {
  return(
   <Fragment>
-    Notes...
+   <div className="notes" id="notes">
+    <Menu
+     pageWrapId={"notes-editor"}
+     // eslint-disable-next-line react/prop-types
+     isOpen={props.isOpen}
+     // eslint-disable-next-line react/prop-types
+     onStateChange={(state) => props.setIsOpen(state.isOpen)}
+     disableAutoFocus
+     outerContainerId={"notes"}
+     customBurgerIcon={false}
+     customCrossIcon={false}
+    >
+     <div>
+      <div className="columns">
+        Search...
+      </div>
+     </div>
+     <p>List...</p>
+    </Menu>
+
+
+    <div size={12} className="notes-editor" id="notes-editor">
+      Editor...
+    </div>
+   </div>
   </Fragment>
   )
 }
