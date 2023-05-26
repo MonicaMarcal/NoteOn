22b9767ca516cf2a1f3831d5101bcc7703460389
Mônica Marçal <monicagmarcal@outlook.com>
2023-05-26 17:19:45 -0300

    style
    header

--- javascript_note/src/components/header/index.jsx
+++ javascript_note/src/components/header/index.jsx
@@ -3,28 +3,30 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-        <section>
-          <header className="bg-white space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">
-            <div className="flex items-center justify-between">
-              <Link to="/">
-                <h1 className="font-semibold text-slate-900">NOTEON</h1>
-              </Link>
-
-              <div className="flex gap-2 items-center justify-between">
-              <a className="hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
-                <Link to="/register">
-                      Register
-                </Link>
-              </a>
-              <a className="hover:bg-blue-400 group flex  items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
-                <Link to="/login">
-                    Login
-                </Link>
-              </a>
-              </div>
+          <header className="bg-violet-800 text-white sticky top-0 z-10">
+            <section className="max-w-4xl mx-auto p-4 flex justify-between items-center">
+            <Link to="/">
+             <h1 className="text-3xl font-medium">
+                📄 NOTEON
+              </h1>
+              </Link>   
+            <div>
+                <button id="mobile-open-button" className="text-3xl sm:hidden focus:outline-none">
+                    &#9776;
+                </button>
+                <nav className="hidden sm:block space-x-8 text-xl" aria-label="main">
+                    <Link to="/register">
+                    <a className="hover:opacity-90">Register</a>
+                    </Link>
+                    <a className="hover:opacity-90">
+                      <Link to="/login">
+                        Login
+                      </Link>
+                    </a>
+                </nav>
             </div>
-          </header>
-      </section>
+        </section>    
+    </header>
   );
 }
 
--- javascript_note/src/screens/home/index.jsx
+++ javascript_note/src/screens/home/index.jsx
@@ -1,30 +1,38 @@
 import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+//import NoteImage from '../../assets/images/back.jpg';
 import Header from "../../components/header";
-
 
 const HomeScreen = () => (
  <Fragment>
-    <Header/>
-    <section className="hero is-dark is-fullheight">
-             <div className="hero-body">
-                 <div className="container has-text-centered">
-                    <div className="column is-6 is-offset-3">
-                        <h1 className="title">
-                        Welcome to NoteOn
-                        </h1>
-                        <h2 className="subtitle">
-                        Create notes easily and access when you wants on the cloud!
-                        </h2>
-                        <div>
-                            <Link to="/register" className="button is-outlined is-black is-large">
-                                <strong>Register for free Nowstrong</strong>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+    <Header/>  
+    <div className=" min-h-screen space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">         
+         <div className="flex items-center w-full w-auto">
+          <div className="max-w-2xl mb-8">
+            <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
+                    Welcome to NoteOn
+            </h1>
+            <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
+            Create notes easily and access when you wants on the cloud!
+            </p>
+
+            <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
+                 <button type="button" className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md'
+                        "aria-label="to register">
+                    <Link to="/register">
+                        <strong>Register for free Nowstrong</strong>
+                    </Link>
+                 </button>              
             </div>
-    </section>
+          </div>
+          {/* <div className="flex items-center justify-center w-full w-3/12 p-3 m-auto">
+          <img src={NoteImage} alt=""/>
+        </div> */}
+        </div>
+       
+      
+    </div>
+
  </Fragment>
 )
 
