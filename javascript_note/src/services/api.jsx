8c0f44464ff6f514dddb09825cf230b3c047634a
Mônica Marçal <monicagmarcal@outlook.com>
2023-06-22 17:26:16 -0300

    api

--- javascript_note/src/services/api.jsx
+++ javascript_note/src/services/api.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-const Api = axios.create({baseURL: "http://localhost:3001"})
+//const Api = axios.create({baseURL: "http://localhost:3001"})
+const Api = axios.create({baseURL: "http://note-on-api.vercel.app"})
 
 export default Api;
